74324a9f6f37915b886e799056580e134e6fae7c
BilyanaNikiforova <biliana_nikifforova@abv.bg>
2021-05-25 14:01:16 +0300

    DataBase

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -15,34 +15,6 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
-    @FXML
-    private Label errorText;
-    @FXML
-    private TextField emailText;
-    @FXML
-    private PasswordField enterPasswordField;
-    @FXML
-    private Button loginButton;
+public class LoginController  {
 
-
-    public void loginButton() {
-        errorText.setText("Wrong email or password");
-        if(emailText.getText().isBlank() == false && enterPasswordField.getText().isBlank()== false){
-            validateLogin();
-
-        } else {
-            errorText.setText("Wrong email or password");
-
-        }
-    }
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
-    public void validateLogin(){
-
-    }
 }
