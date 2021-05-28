9866b451d4eca04bfe1fa17167c64dcc70e65e98
BilyanaNikiforova <biliana_nikifforova@abv.bg>
2021-05-28 17:15:33 +0300

    InsertData

--- src/sample/RegistrationController.java
+++ src/sample/RegistrationController.java
@@ -1,23 +1,13 @@
 package sample;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.*;
-import oracle.jdbc.driver.*;
-
 
 public class RegistrationController {
 
@@ -33,7 +23,7 @@ public class RegistrationController {
     private Label reg_errorid;
     Main s = new Main();
 
-    public void registrationButtonOnAction() throws IOException {
+    public void registrationButtonOnAction() throws IOException, SecurityException {
 
 
         if (email_id.getText().isEmpty() && password_id.getText().isEmpty() && repassword_id.getText().isEmpty()) {
@@ -42,22 +32,16 @@ public class RegistrationController {
         } else if (!repassword_id.getText().equals(password_id.getText())) {
             reg_errorid.setText("Грешно въведени данни!");
         } else {
-            //  User user=new User(email_id, password_id);
-            // user.
-
             String username = email_id.getText();
             String pass = password_id.getText();
             String pass2 = repassword_id.getText();
 
-            try {
 
-                //String DBURL = "jdbc:oracle:thin:@localhost:1521:xe";
-                //String username1 = "USP";
-                //String password = "usp";
+            try {
                 Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","USP","usp");
-                Statement stmt = conn.createStatement();
-                String insertQuery="INSERT INTO USERS(ID_USER,E-mail,PASSWORD) VALUES('"+pass2+"','"+username+"','"+pass+"')";
-                stmt.executeUpdate(insertQuery);
+                Statement statement = conn.createStatement();
+                String insertQuery=("INSERT INTO USERS VALUES ('"+pass2+"','"+username+"','"+pass+"')");
+                statement.executeUpdate(insertQuery);
                 System.out.println("done");
 
             } catch (Exception e) {
